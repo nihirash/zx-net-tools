2e5309fb387a4b7c7fefa8a201dcb784b0da934f
Andy Karpov <andykarpov@users.noreply.github.com>
2023-04-10 00:43:41 +0300

    Added karabas-pro target

--- uGophy/main.asm
+++ uGophy/main.asm
@@ -13,7 +13,7 @@
 ;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
     DEVICE ZXSPECTRUM128
-    org 24100
+    org #6000
 Start: 
     di
     res 4, (iy+1)
@@ -22,7 +22,11 @@ stack_pointer = #5aff
     call checkHighMem : jp nz, noMem
     
     xor a : out (#fe), a : call changeBank
+    IFDEF TRDOS
+    ld sp, $ ; We don't care about anything before    
+    ELSE    
     ld de, #4000 : ld bc, eop - player : ld hl, player : ldir
+    ENDIF
     ELSE
     jp zx48start
     ds 128
@@ -38,6 +42,25 @@ zx48start:
     IFDEF PLUS3DOS
     call initDos
     ENDIF
+
+    IFDEF TRDOS
+    ld hl, init1 : call putStringZ
+
+    call Dos.init
+    ld de, work_dir : call Dos.cwd
+    or a : jp nz, .dirError
+
+    ld hl, init2 : call putStringZ
+    ; Read player from disk
+    ld l, Dos.FA_READ, bc, player_name : call Dos.fopen
+    ld bc, #4000, de, 3061 : call Dos.fread
+    call Dos.fclose
+
+    xor a : ld (#5c6a), a  ; Thank you, Mario Prato, for feedback
+    ei
+
+    ld hl, init3 : call putStringZ
+    ENDIF
     
     IFNDEF SPECTRANET
     call initWifi
@@ -48,6 +71,12 @@ zx48start:
     ld de, path : ld hl, server : ld bc, port : call openPage
 
     jp showPage
+.dirError
+    ld hl, .err : call putStringZ
+    jr $
+
+.err db "Can't enter 'wifi' directory",13,"Computer halted",0
+
     IFNDEF ZX48
 noMem:
     ld hl, no128k
@@ -61,6 +90,9 @@ nmLp:
     jp nmLp
     ENDIF
 
+init1 db "Initing sd card", 13, 0
+init2 db "Loading PT3 player", 13, 0
+init3 db "Initing Wifi module", 13, 0
 
 wSec: ei : ld b, 50
 wsLp  halt : djnz wsLp
@@ -98,7 +130,10 @@ wsLp  halt : djnz wsLp
     include "spectranet.asm"
     ENDIF
 
-    
+work_dir db "wifi",0
+
+player_name db "player.bin", 0
+
 open_lbl db 'Opening connection to ', 0
 
 path    db '/uhello'
@@ -108,7 +143,14 @@ server  db 'nihirash.net'
 port    db '70'
         defs 5
         db 0
+
+    IFDEF TRDOS
+    include "dos/ochkodos.asm"
+page_buffer equ Dos.bin
+    ELSE
 page_buffer equ $
+    ENDIF
+
     display "PAGE buffer:", $
     IFNDEF ZX48
 no128k  db 13, "You're in 48k mode!", 13, 13
@@ -117,14 +159,19 @@ no128k  db 13, "You're in 48k mode!", 13, 13
         db     "System halted!", 0
     ENDIF
     IFNDEF ZX48
+
 player 
-    DISPLAY "Player starts:" , $       
+    IFNDEF TRDOS
+    DISPLAY "Player starts:" , $
     include "vtpl.asm"
     DISPLAY "Player ends: ", $
     ENT
+    ENDIF
     ENDIF
 eop equ $
     SAVEBIN "ugoph.bin", Start, $ - Start
     
     SAVETAP "ugoph.tap", Start
+
+    SAVEHOB "ugoph.$c", "ugoph.C", Start, $ - Start
     
